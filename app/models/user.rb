eb03ef94c87aee3dc2f10be60eaf9064a1c3bde3
Musa V. Ahmed <mahme012@fiu.edu>
2014-10-16 09:46:53 -0400

    6.35

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ActiveRecord::Base
+    # downcase email for db
+    before_save { self.email = email.downcase }
+
+    # name validation
     validates :name,  prescence: true, length: { maximum: 50}
 
+    # email validation
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, prescence: true, format { with: VALID_EMAIL_REGEX },
                       uniqueness: {case_sensitive: false }
 
-    before_save { self.email = email.downcase }
+    has_secure_password
 
+    # password validation
+    validates :password, length: { minimum: 6}
+    
     has_many :models
 end
