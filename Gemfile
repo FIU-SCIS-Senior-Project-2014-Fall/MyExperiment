8bd5a2b0033209e2490f4f2aa2fd34c92dde7d20
Musa V. Ahmed <mahme012@fiu.edu>
2014-10-16 09:32:44 -0400

    Thursday 9:30AM

--- Gemfile
+++ Gemfile
@@ -44,3 +44,5 @@ gem 'coveralls', require: false
 gem 'bootstrap-sass', '2.3.2.0'
 
 gem 'sprockets', '2.11.0'
+
+gem 'bcrypt-ruby', '3.1.2'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+    validates :name,  prescence: true, length: { maximum: 50}
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, prescence: true, format { with: VALID_EMAIL_REGEX },
+                      uniqueness: {case_sensitive: false }
+
+    before_save { self.email = email.downcase }
+
     has_many :models
 end
