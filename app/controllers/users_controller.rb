4a235d550c98fe8338dccb05a40104cac9c36105
Musa V. Ahmed <mahme012@fiu.edu>
2014-10-16 11:27:26 -0400

    start 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params) 
       if @user.save
-          #success
+          flash[:success] = "Welcome to MyExperiment!"
+          redirect_to @user          
       else
           render 'new'
       end
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
   private
 
     def user_params
-        params.require(:user).permit(:name, :email:, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
