8efa745a7db0de316d0d58a6c835dbbbf1713af5
Musa V. Ahmed <mahme012@fiu.edu>
2014-10-16 15:09:21 -0400

    fix exp

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -2,6 +2,6 @@ class Experiment < ActiveRecord::Base
     belongs_to :user
     default_scope -> { order('created_at DESC') }
     validates :type, presence: true
-    validates :nodes, presence: true, length { minimum: 3 } 
+    validates :nodes, presence: true, length: { minimum: 3 } 
     validates  :user_id, presence: true
 end
