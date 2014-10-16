89abeb5580df4fff189643f86e58c5806aa2680b
Musa V. Ahmed <mahme012@fiu.edu>
2014-10-16 12:11:31 -0400

    8.2.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     # downcase email for db
     before_save { self.email = email.downcase }
+    before_create :create_remember_token    
 
     # name validation
     validates :name,  presence: true, length: { maximum: 50 }
@@ -16,4 +17,17 @@ class User < ActiveRecord::Base
     validates :password, length: { minimum: 6}
     
     has_many :models
+
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def User.digest(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+        def create_remember_token
+            self.remember_token = User.digest(User.new_remember_token)
+        end
 end
